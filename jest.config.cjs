67dece4c9a1fc1f8c1918878792ff57f3145426a
patsoulkitchen <pat@soulkitchenai.com>
2026-01-15 17:12:54 -0800

    jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,20 +3,4 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    extensionsToTreatAsEsm: ['.ts'],
-    transform: {
-        '^.+\\.ts$': ['ts-jest', { useESM: true }],
-    },
-    moduleNameMapper: {
-        '^(\\...+?)\\.js$': '$1',
-    },
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
-    },
-    testEnvironmentOptions: {
-        runScripts: 'unsafe',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
