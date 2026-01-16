c0e86b2a1a062a7f3361e2ee9d8df217825dfbaa
patsoulkitchen <pat@soulkitchenai.com>
2026-01-15 17:10:09 -0800

    jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,25 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.ts$': ['ts-jest', { useESM: true }],
+    },
+    moduleNameMapper: {
+        '^(\\...+?)\\.js$': '$1',
+    },
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    runner: 'jest-node',  // Optional, but helps with ESM
+    testRunner: 'jest-circus/runner',
+    // Add this for experimental-vm-modules
+    testEnvironmentOptions: {
+        runScripts: 'unsafe',
+    },
+    // Run with node flags
+    testRunner: 'jest-circus/runner',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],  // We'll add this file next
 };
